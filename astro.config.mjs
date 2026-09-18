90efe2118ffa4fcc83c61dfcdab75a72d05f1087
David <davidmel7562@gmail.com>
2026-09-18 21:29:01 +0200

    ci: add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 export default defineConfig({
-    site: 'https://tjaugust01.github.io',
-    base: '/dotcv',
+  site: "https://thfoxy.github.io",
+  base: "/cv",
 });
